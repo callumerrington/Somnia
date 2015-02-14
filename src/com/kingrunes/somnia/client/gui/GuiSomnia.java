fadf24061ba79f461b857ba8880c615601918405
kingrunes <callum.errington@gmail.com>
2015-02-14 23:22:54 +0000

    - Redesigned GUI (not finished, but much nicer) - GUI now includes progress bar and ETA - Cleaned up the GUI rendering code

--- src/com/kingrunes/somnia/client/gui/GuiSomnia.java
+++ src/com/kingrunes/somnia/client/gui/GuiSomnia.java
@@ -1,13 +1,11 @@
 package com.kingrunes.somnia.client.gui;
 
-import static org.lwjgl.opengl.GL11.glDisable;
-import static org.lwjgl.opengl.GL11.glPopMatrix;
-import static org.lwjgl.opengl.GL11.glPushMatrix;
-import static org.lwjgl.opengl.GL11.glScalef;
-
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiSleepMP;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.item.Item;
@@ -15,25 +13,27 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.client.C0BPacketEntityAction;
 import net.minecraft.util.ChatComponentTranslation;
 
-import org.lwjgl.opengl.GL11;
-
+import com.kingrunes.somnia.Somnia;
 import com.kingrunes.somnia.common.StreamUtils;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
+import static org.lwjgl.opengl.GL11.*;
+
 @SideOnly(Side.CLIENT)
 public class GuiSomnia extends GuiSleepMP
 {
-	public static final String	TRANSLATION_FORMAT = "somnia.status.%s",
-								SPEED_FORMAT = "%sx%s";
-	
 	public static final String 	COLOR = new String(new char[]{ (char)167 }),
 								BLACK = COLOR+"0",
 								WHITE = COLOR+"f",
 								RED = COLOR+"c",
 								DARK_RED = COLOR+"4",
 								GOLD = COLOR+"6";
+	
+	public static final String	TRANSLATION_FORMAT = "somnia.status.%s",
+			SPEED_FORMAT = "%sx%s",
+			ETA_FORMAT = WHITE + "(%s:%s)";
 	
 	public static final byte[]	BYTES_WHITE = new byte[]{ (byte) 255, (byte) 255, (byte) 255 },
 								BYTES_DARK_RED = new byte[]{ (byte) 171, 0, 0 },
@@ -42,11 +42,12 @@ public class GuiSomnia extends GuiSleepMP
 	
 	private static RenderItem presetIconRenderer = new RenderItem();
 	private static ItemStack clockItemStack = new ItemStack(Item.getItemById(347));
-								
+
+	private List<Double> speedValues = new ArrayList<Double>();
 	
 	public String status = "Waiting...";
 	public double speed = 0;
-	public int sleeping = 0, awake = 0;
+	public long startTicks = -1l;
 	
 	@Override
 	public boolean doesGuiPauseGame()
@@ -67,43 +68,118 @@ public class GuiSomnia extends GuiSleepMP
     {
 		super.drawScreen(par1, par2, par3);
 		
-		glPushMatrix();
+		boolean currentlySleeping = speed != .0d;
+		if (currentlySleeping)
 		{
-			glScalef(4f, 4f, 1f);
-			presetIconRenderer.renderItemIntoGUI
-			(
-				fontRendererObj,
-				this.mc.renderEngine,
-				clockItemStack,
-				(this.width - (this.width / 5))/4,
-				((this.height / 2)-36)/4
-			);
+			if (startTicks == -1l)
+				startTicks = mc.theWorld.getTotalWorldTime();
 		}
-		glPopMatrix();
-		glDisable(GL11.GL_LIGHTING);
+		else
+			startTicks = -1l;
+		
+
+		/*
+		 * GL stuff
+		 */
+		glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+		glDisable(GL_LIGHTING);
+		glDisable(GL_FOG);
+		
+		/*
+		 * Progress bar
+		 * Multiplier
+		 * ETA
+		 * Clock
+		 */
+		if (startTicks != -1l && Somnia.clientAutoWakeTime != -1)
+		{
+			// Progress Bar
+			this.mc.getTextureManager().bindTexture(Gui.icons);
+			
+			double 	rel = mc.theWorld.getTotalWorldTime()-startTicks,
+					diff = Somnia.clientAutoWakeTime-startTicks,
+					progress = rel / diff;
+			
+			int 	x = 20,
+					maxWidth = (this.width-(x*2));
+			
+			glEnable(GL_BLEND);
+			glColor4f(1.0f, 1.0f, 1.0f, .2f);
+			renderProgressBar(x, 10, maxWidth, 1.0d);
+			
+			glDisable(GL_BLEND);
+			glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+			renderProgressBar(x, 10, maxWidth, progress);
+			
+			// Multiplier
+			renderScaledString(x, 20, 1.5f, SPEED_FORMAT, getColorStringForSpeed(speed), speed);
+			
+			// ETA
+			double total = 0.0d;
+			for (double value : speedValues)
+				total += value;
+			double avg = total / speedValues.size();
+			int etaTotalSeconds = (int)((diff-rel) / (avg*20)); // remaining ticks / (average multiplier * standard tick rate)
+			
+			int etaSeconds = etaTotalSeconds % 60,
+				etaMinutes = (etaTotalSeconds-etaSeconds) / 60;
+			
+			renderScaledString(x + 50 + 10, 20, 1.5f, ETA_FORMAT, (etaMinutes>10?"":"0") + etaMinutes, (etaSeconds>10?"":"0") + etaSeconds);
+			
+			// Clock
+			renderClock(maxWidth - 40, 30, 4.0f);
+		}
+    }
+	
+	private void renderProgressBar(int x, int y, int maxWidth, double progress)
+	{
+		int amount = (int) (progress * maxWidth);
+		while (amount > 0)
+		{
+			this.drawTexturedModalRect(x, y, 0, 69, (amount > 180 ? 180 : amount), 5);
+			
+			amount -= 180;
+			x += 180;
+		}
+	}
+	
+	private int renderScaledString(int x, int y, float scale, String format, Object... args)
+	{
+		String str = String.format(format, args);
 		glPushMatrix();
 		{
-			glScalef(2f, 2f, 1f);
+			glTranslatef(x, 20, 0.0f);
+			glScalef(scale, scale, 1.0f);
 			drawString
 			(
 				fontRendererObj,
-				String.format(SPEED_FORMAT, getColorStringForSpeed(speed), speed),
-				5,
-				5,
+				str,
+				0,
+				0,
 				Integer.MIN_VALUE
 			);
 		}
 		glPopMatrix();
 		
-		boolean b = status.length() != 5; // cheap way of testing if the string is a time string
-		drawString(
+		return (int) (fontRendererObj.getStringWidth(str) * scale);
+	}
+	
+	private void renderClock(int x, int y, float scale)
+	{
+		glPushMatrix();
+		{
+			glTranslatef(x, y, 0.0f);
+			glScalef(scale, scale, 1.0f);
+			presetIconRenderer.renderItemIntoGUI
+			(
 				fontRendererObj,
-				WHITE+status,
-				b ? this.width/2 - fontRendererObj.getStringWidth(status)/2 : this.width - (this.width / 5) + 18,
-				this.height / (b?8:2),
-				Integer.MIN_VALUE
-				);
-    }
+				this.mc.renderEngine,
+				clockItemStack,
+				0, 0
+			);
+		}
+		glPopMatrix();
+	}
 	
 	public void readField(DataInputStream in) throws IOException
 	{
@@ -111,6 +187,9 @@ public class GuiSomnia extends GuiSleepMP
 		{
 		case 0x00:
 			speed = in.readDouble();
+			speedValues.add(speed);
+			if (speedValues.size() > 5)
+				speedValues.remove(0);
 			break;
 		case 0x01:
 			String str = StreamUtils.readString(in);
