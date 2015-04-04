729fed46917737b6713080854f4ebd2db502bee7
kingrunes <callum.errington@gmail.com>
2015-04-04 15:41:59 +0100

    - Added SomniaVersion.isHigher in preparation for auto-updater

--- src/com/kingrunes/somnia/SomniaVersion.java
+++ src/com/kingrunes/somnia/SomniaVersion.java
@@ -31,4 +31,25 @@ public class SomniaVersion
 	{
 		return String.format(FORMAT, CORE_MAJOR_VERSION, CORE_MINOR_VERSION, CORE_REVISION_VERSION, BUILD);
 	}
+	
+	public static boolean isHigher(String versionString)
+	{
+		String[] segments = versionString.split("\\.");
+		if (segments.length < 3)
+			return false;
+		int[] intArray = new int[segments.length];
+		for (int i=0; i<segments.length; i++)
+		{
+			try
+			{
+				intArray[i] = Integer.parseInt(segments[i]);
+			}
+			catch (NumberFormatException nfe)
+			{
+				nfe.printStackTrace();
+				return false;
+			}
+		}
+		return (MAJOR_VERSION < intArray[0] ? true : (MINOR_VERSION < intArray[1] ? true : (REVISION_VERSION < intArray[2] ? true : intArray.length >= 4 && BUILD < intArray[3])));
+	}
 }
