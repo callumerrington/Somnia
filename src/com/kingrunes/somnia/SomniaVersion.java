f8808ea573447638cb12b5519c1b989d7db77b09
kingrunes <callum.errington@gmail.com>
2015-08-08 15:51:53 +0100

    - Bumped Somnia and SomniaCore version to 1.5.0 for MC-1.8

--- src/com/kingrunes/somnia/SomniaVersion.java
+++ src/com/kingrunes/somnia/SomniaVersion.java
@@ -5,17 +5,17 @@ public class SomniaVersion
 							// Incremented when a significant change to the mod is made, never reset
 	public static final int MAJOR_VERSION = 1,
 							// Incremented for new features or significant rewrites, reset with every new MAJOR_VERSION
-							MINOR_VERSION = 4,
+							MINOR_VERSION = 5,
 							// Incremented when a bugfix is made and the mod can be considered 'stable', reset with every new MINOR_VERSION
-							REVISION_VERSION = 2;
+							REVISION_VERSION = 0;
 							// Incremented automatically by the build system, never reset
 	
 							// Incremented when a significant change to the mod is made, never reset
 	public static final int CORE_MAJOR_VERSION = 1,
 							// Incremented for new features or significant rewrites, reset with every new MAJOR_VERSION
-							CORE_MINOR_VERSION = 3,
+							CORE_MINOR_VERSION = 5,
 							// Incremented when a bugfix is made and the mod can be considered 'stable', reset with every new MINOR_VERSION
-							CORE_REVISION_VERSION = 1;
+							CORE_REVISION_VERSION = 0;
 							// Incremented automatically by the build system, never reset
 
 	public static final int BUILD = 0;
